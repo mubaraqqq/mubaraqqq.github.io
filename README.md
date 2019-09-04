ce56f6ea01e9f90287351447fad1f35241ffe333
Mubaraqqq <ayomidemusty@gmail.com>
2019-09-04 09:21:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# StartNG-Task-2
+
+lucid post - https://lucid.blog/ayomidemusty/post/1566581658;
+twitter post- https://twitter.com/mubaraq__/status/1164917631884963840
---
+++ script.js
@@ -0,0 +1,26 @@
+function formValidate(){
+    var name = document.getElementById('formname').value;
+    var email = document.getElementById('formemail').value;
+    var title = document.getElementById('formtitle').value;
+    var message = document.getElementById('formmessage').value;
+    var regex = /[0-9]/;
+    var regexx;
+    if (name.length < 4) {
+        alert ('Your Name shouldn\'t be less than 4 words');
+        return false;
+    } else if (message.length < 20) {
+        alert('Your messsage shouldn\'t be less than 20 words');
+        return false;
+    } else if (name.match(regex)) {
+        alert('Name can\'t contain numbers');
+         return false;
+    } else if (title.match(regex)) {
+        alert('Title can\'t contain numbers');
+         return false;
+    } else if (message.match(regex)) {
+        alert('Messages can\'t contain numbers');
+         return false;
+    }
+    
+}
+
